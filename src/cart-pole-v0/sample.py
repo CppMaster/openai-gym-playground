9049569a77ca581cfd2fe286c558676e57712f64
Patryk Steckel <patryksteckel@gmail.com>
2022-05-20 08:57:29 +0200

    -updated requirements -updated cart-pole-v0 sample

--- src/cart-pole-v0/sample.py
+++ src/cart-pole-v0/sample.py
@@ -1,22 +1,58 @@
 import gym
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+
+# load environment
 env = gym.make('CartPole-v0')
 
+# make it deterministic
+seed = 42
+env.seed(seed)
+env.action_space.seed(seed)
+
+# track total reward for each episode
 total_rewards = []
 
-for i_episode in range(20):
+# run 10 episodes
+for i_episode in range(10):
+
+    # start the episode
     observation = env.reset()
-    t = 0
-    for t in range(1000):
+    episode_reward = 0.0
+
+    # run maximum of 200 steps
+    for timestep in range(200):
+
+        # render current step
         env.render()
         print(observation)
+
+        # choose a random action
         action = env.action_space.sample()
+
+        # perform the action
         observation, reward, done, info = env.step(action)
+
+        # accumulate the reward
+        episode_reward += reward
+
+        # end the episode
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print(f"Episode finished after {timestep + 1} timesteps. Reward: {episode_reward}")
             break
-    total_rewards.append(t)
+
+    total_rewards.append(episode_reward)
+
 env.close()
 
-print(np.mean(total_rewards))
+# show statistics
+mean_total_reward = np.mean(total_rewards)
+print(f"Mean total reward: {mean_total_reward}")
+
+# plot rewards
+g = sns.lineplot(range(len(total_rewards)), total_rewards)
+# draw mean line
+plt.axhline(mean_total_reward, c="r")
+plt.show()
