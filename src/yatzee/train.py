b621c32eb3a98742dc7a580e0ffc67650241a644
Patryk Steckel <patryksteckel@gmail.com>
2023-03-06 17:00:49 +0100

    bigger model

--- src/yatzee/train.py
+++ src/yatzee/train.py
@@ -1,14 +1,13 @@
 from sb3_contrib import MaskablePPO
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.monitor import Monitor
+import torch as th
 
 from src.yatzee.valid_observation_env import YahtzeeValidObservationEnv
 
-# env = YahtzeeValidObservationEnv()
 env = make_vec_env(lambda: YahtzeeValidObservationEnv(), n_envs=16)
 
-old_model = MaskablePPO.load("models/vec_env_8")
-model = MaskablePPO("MultiInputPolicy", env, verbose=True, tensorboard_log="logs/ven_env_16_ent_001",
-                    ent_coef=0.01, n_steps=2**12)
-model.policy = old_model.policy
+model = MaskablePPO("MultiInputPolicy", env, verbose=True, tensorboard_log="logs/leakyrelu_arch-256-256-256",
+                    ent_coef=0.01, n_steps=2 ** 13, batch_size=2 ** 9, policy_kwargs=dict(
+        activation_fn=th.nn.LeakyReLU, net_arch=dict(pi=[256, 256, 256], vf=[256, 256, 256])
+    ))
 model.learn(total_timesteps=1000000000)
